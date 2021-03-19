2bcdd5db58420b70241230755daf04d428938e9f
mishra.shashank35@gmail.com <mishra.shashank35@gmail.com>
2021-03-19 12:36:15 +0530

    Add endpoint with restaurant by name

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
@@ -4,10 +4,12 @@ import com.upgrad.FoodOrderingApp.api.model.*;
 import com.upgrad.FoodOrderingApp.service.businness.CategoryService;
 import com.upgrad.FoodOrderingApp.service.businness.RestaurantService;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,22 @@ public class RestaurantController {
         List<RestaurantEntity> restaurants = restaurantService.getAllRestaurants();
 
         //Declare list of RestaurantListResponse
+        return getRestaurantListResponseEntity(restaurants);
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+            value = "/restaurant/name/{restaurant_name}",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<RestaurantList>> getRestaurantsByName(@PathVariable("restaurant_name") final String restaurantName)
+    throws RestaurantNotFoundException {
+        //Get all restaurants as a list of RestaurantEntity
+        List<RestaurantEntity> restaurants = restaurantService.getRestaurantsByName(restaurantName);
+
+        //Declare list of RestaurantListResponse
+        return getRestaurantListResponseEntity(restaurants);
+    }
+
+    private ResponseEntity<List<RestaurantList>> getRestaurantListResponseEntity(List<RestaurantEntity> restaurants) {
         List<RestaurantList> restaurantListResponse = new ArrayList<>();
 
         for (RestaurantEntity restaurantEntity: restaurants) {
@@ -55,6 +73,4 @@ public class RestaurantController {
 
         return new ResponseEntity<List<RestaurantList>>(restaurantListResponse, HttpStatus.OK);
     }
-
-
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -2,9 +2,11 @@ package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,6 +20,28 @@ public class RestaurantService {
      */
     public List<RestaurantEntity> getAllRestaurants() {
         return restaurantDao.getAllRestaurants();
+    }
+
+    /**
+     * @return List of all restaurants
+     */
+    public List<RestaurantEntity> getRestaurantsByName(final String restaurantName)
+    throws RestaurantNotFoundException {
+        //If the restaurant name field entered by the customer is empty, throw exception
+        if(restaurantName == null || restaurantName == "") {
+            throw new RestaurantNotFoundException("RNF-003", "Restaurant name field should not be empty");
+        }
+
+        //Fetch all restaurants by provided name
+        List<RestaurantEntity> restaurantEntities = restaurantDao.getRestaurantByName(restaurantName);
+
+        //If there are no restaurants by the name entered by the customer, return an empty list
+        if(restaurantEntities == null) {
+            return new ArrayList<>();
+        }
+        else {
+            return restaurantEntities;
+        }
     }
 
 
