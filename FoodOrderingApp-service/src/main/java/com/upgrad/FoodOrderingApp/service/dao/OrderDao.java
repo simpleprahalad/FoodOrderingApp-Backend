a9e1941eb85c289ae067796e6b00d23c5ea37f5a
Shyam Sunder Grover <post2shyam@gmail.com>
2021-03-21 17:30:06 +0530

    Added dap and entities

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/OrderDao.java
@@ -0,0 +1,24 @@
+package com.upgrad.FoodOrderingApp.service.dao;
+
+import com.upgrad.FoodOrderingApp.service.entity.OrdersEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class OrderDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<OrdersEntity> getAllOrdersOfCustomerByUuid(final String customerUuid) {
+        try {
+            return entityManager.createNamedQuery("getAllOrdersOfCustomerByUuid", OrdersEntity.class).setParameter("customerUuid", customerUuid).getResultList();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+}
---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/OrdersEntity.java
@@ -0,0 +1,145 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@NamedQueries(
+        {
+                @NamedQuery(name = "getAllOrdersOfCustomerByUuid", query = "select o from OrdersEntity o where o.customer.uuid=:customerUuid"),
+
+        }
+)
+public class OrdersEntity implements Serializable {
+    @Id
+    @Column(name = "id")
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "uuid")
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "bill")
+    @NotNull
+    private BigDecimal bill;
+
+    @OneToOne
+    @JoinColumn(name = "coupon_id", nullable = true)
+    private CouponEntity coupon;
+
+    @Column(name = "discount")
+    private BigDecimal discount;
+
+    @Column(name = "date")
+    @NotNull
+    private Date date;
+
+    @OneToOne
+    @JoinColumn(name = "payment_id", nullable = true)
+    private PaymentEntity payment;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private CustomerEntity customer;
+
+    @OneToOne
+    @JoinColumn(name = "address_id", nullable = false)
+    private AddressEntity address;
+
+    @OneToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private RestaurantEntity restaurant;
+
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public CouponEntity getCoupon() {
+        return coupon;
+    }
+
+    public void setCoupon(CouponEntity coupon) {
+        this.coupon = coupon;
+    }
+
+    public PaymentEntity getPayment() {
+        return payment;
+    }
+
+    public void setPayment(PaymentEntity payment) {
+        this.payment = payment;
+    }
+
+    public CustomerEntity getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerEntity customer) {
+        this.customer = customer;
+    }
+
+    public AddressEntity getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressEntity address) {
+        this.address = address;
+    }
+
+    public RestaurantEntity getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(RestaurantEntity restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public BigDecimal getBill() {
+        return bill;
+    }
+
+    public void setBill(BigDecimal bill) {
+        this.bill = bill;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
